dc8a5e49493c089ba1aa0e729155c1e9e190c116
mikoto2000 <mikoto2000@gmail.com>
2024-04-04 15:28:31 +0900

    Added build scripts for nexe.

---
+++ build_linux_by_nexe.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# build_linux_by_nexe.bash
+#
+# Usage:
+#     build_linux_by_nexe.bash TAG_NAME
+#
+
+# 引数チェック
+if [ "${1}" = '' ]; then
+  echo "Usage: build_linux_by_nexe.bash TAG_NAME NODE_VERSION"
+  exit 1
+fi
+
+if [ "${2}" = '' ]; then
+  echo "Usage: build_linux_by_nexe.bash TAG_NAME NODE_VERSION"
+  exit 1
+fi
+
+APP_VERSION="${1}"
+NODE_VERSION="${2}"
+OS="linux"
+ARCH="x64"
+APP_SUFFIX="${OS}-${ARCH}-${APP_VERSION}"
+NEXE_TARGET="${OS}-${ARCH}-${NODE_VERSION}"
+
+CURRENT_DIR=$(cd $(dirname $0);pwd)
+
+APP_NAME="devcontainer"
+BIN_PATH="../dist/devcontainer-${APP_SUFFIX}"
+
+# compile devcontainers/cli
+cd upstream
+yarn
+yarn compile-prod
+
+# create single executable binary by nexe
+npx -y nexe -i ./devcontainer.js --make="-j$(nproc)" -r="scripts/updateUID.Dockerfile" -t ${SUFFIX} -b -o ${BIN_PATH}
+
---
+++ build_windows_by_nexe.ps1
@@ -0,0 +1,37 @@
+﻿# build_windows.ps1
+#
+# 引数:
+#     AppVersion: ビルドしたバイナリのふぃある名に付与するバージョン番号
+
+Param(
+    [string]$AppVersion,
+    [string]$NodeVersion
+)
+
+# AppVersion が指定されていなかった場合即終了する
+if ([String]::IsNullOrEmpty($AppVersion)) {
+    Write-Error "'AppVersion' が設定されていません。"
+    return 1
+}
+if ([String]::IsNullOrEmpty($NodeVersion)) {
+    Write-Error "'NodeVersion' が設定されていません。"
+    return 1
+}
+
+$OS="windows"
+$ARCH="x64"
+$APP_SUFFIX="${OS}-${ARCH}-${AppVersion}"
+$NEXE_TARGET="${OS}-${ARCH}-${NodeVersion}"
+
+$CURRENT_DIR = . Split-Path -Parent $MyInvocation.MyCommand.Path
+
+$APP_NAME="devcontainer"
+$BIN_PATH="../dist/devcontainer-${APP_SUFFIX}.exe"
+
+cd upstream
+npm i -g yarn
+yarn
+yarn compile-prod
+
+npx -y nexe -i ./devcontainer.js -r="scripts/updateUID.Dockerfile" -t ${NEXE_TARGET} -b -o ${BIN_PATH}
+
