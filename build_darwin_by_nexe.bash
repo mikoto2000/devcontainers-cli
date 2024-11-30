f34baa97bb48f9dbf757b66c2914d18d3dee649f
mikoto2000 <mikoto2000@gmail.com>
2024-11-30 09:40:20 +0900

    Added darwin build script.

---
+++ build_darwin_by_nexe.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# build_darwin_by_nexe.bash
+#
+# Usage:
+#     build_darwinlinux_by_nexe.bash TAG_NAME
+#
+
+# 引数チェック
+if [ "${1}" = '' ]; then
+  echo "Usage: build_darwin_by_nexe.bash TAG_NAME NODE_VERSION"
+  exit 1
+fi
+
+if [ "${2}" = '' ]; then
+  echo "Usage: build_darwin_by_nexe.bash TAG_NAME NODE_VERSION"
+  exit 1
+fi
+
+APP_VERSION="${1}"
+NODE_VERSION="${2}"
+OS="darwin"
+ARCH="x64"
+APP_SUFFIX="${OS}-${ARCH}-${APP_VERSION}"
+NEXE_TARGET="${OS}-${ARCH}-${NODE_VERSION}"
+
+CURRENT_DIR=$(cd $(dirname $0);pwd)
+
+APP_NAME="devcontainer"
+BIN_PATH="../dist/devcontainer-${APP_SUFFIX}"
+
+# compile devcontainers/cli
+cd upstream
+yarn
+yarn compile-prod
+
+# create single executable binary by nexe
+npx -y nexe --loglevel="verbose" -i ./devcontainer.js --make="-j$(nproc)" -r="scripts/updateUID.Dockerfile" -t ${SUFFIX} -b -o ${BIN_PATH}
+
